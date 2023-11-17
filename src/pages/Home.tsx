a0cc56edbc2e7ec35be680aa783a4c3c2eb39e06
Júnior Nascimento <junior.nascm@gmail.com>
2023-11-16 21:47:15 -0300

    image relative

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,24 +13,26 @@ import Button from "@src/components/Button";
 const Home: Component = () => {
   return (
     <main>
-      <div class=" text-white py-8 px-4 gap-8 flex flex-col
-                bg-dark-purple relative h-[500px]">
-        <header class="flex flex-row justify-between items-center">
-          <LogoLight />
-          <Button label="Apply for access" secondary />
-        </header>
-        <div id="hero" class="flex flex-col gap-8">
-          <h1 class="font-fraunces wonky text-[3.125rem] text-center">Data <span class="underline decoration-eucaplyptus">tailored</span> to your needs.</h1>
-          <div class="flex items-center justify-center w-full">
-            <Button label="Learn More" />
+      <div class=" text-white flex flex-col">
+        <div class="pt-8 px-4 gap-8  bg-dark-purple relative">
+          <header class="flex flex-row justify-between items-center">
+            <LogoLight />
+            <Button label="Apply for access" secondary />
+          </header>
+          <div id="hero" class="flex flex-col gap-8">
+            <h1 class="font-fraunces wonky text-[3.125rem] text-center">Data <span class="underline decoration-eucaplyptus">tailored</span> to your needs.</h1>
+            <div class="flex items-center justify-center w-full">
+              <Button label="Learn More" />
+            </div>
+          </div>
+          <div class="flex items-center w-full justify-center absolute">
+            <img src={HeroImage} alt="Hero Image" class="w-[320px] h-[184px] sm:w-[514px] sm:h-[296px] md:w-[767px] md:h-[441px] z-20" />
           </div>
         </div>
-        <div class="flex items-center w-full justify-center">
-          <img src={HeroImage} alt="Hero Image" class="lg:w-[44rem] md:w-[29rem] w-[18rem] z-20" />
+        <div class="md:bg-hero-lg bg-hero-sm sm:bg-hero-md bg-no-repeat bg-bottom h-20 bg-cover bg-ghost-white">
         </div>
       </div>
-      <div class="md:bg-hero-lg bg-hero-sm sm:bg-hero-md bg-no-repeat bg-bottom h-20 bg-cover bg-ghost-white">
-      </div>
+
 
       <div class="flex flex-col gap-10 relative bg-ghost-white lg:flex-row lg:pt-[14rem]  lg:pb-20 lg:px-30 ">
         <div class="flex flex-col gap-6 justify-center items-center">
@@ -65,8 +67,8 @@ const Home: Component = () => {
         <img src={Founder} alt="Founder" class="-z-10 relative lg:w-[28rem] lg:h-[28rem]" />
         <div class="flex flex-col gap-6 text-center bg-dark-purple -mt-20 z-10 p-8">
           <div class="flex flex-col gap-4">
-          <h2 class="font-fraunces text-[2rem] font-semibold text-white">Be the first to test</h2>
-          <p class="text-white font-manrope text-center">Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I&apos;ll be in touch to schedule a call.</p>
+            <h2 class="font-fraunces text-[2rem] font-semibold text-white">Be the first to test</h2>
+            <p class="text-white font-manrope text-center">Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I&apos;ll be in touch to schedule a call.</p>
           </div>
           <Button label="Apply for access" />
         </div>
